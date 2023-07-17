9ca2f144c4dbfb3bf9a73c19dbab6e4fa98f136a
Francisco Costa <francisco100eg@gmail.com>
2023-07-17 18:05:31 -0300

    Fix: Finish signup logic

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon2 from 'argon2';
@@ -6,19 +6,24 @@ import * as argon2 from 'argon2';
 @Injectable({})
 class AuthService {
   constructor(private prisma: PrismaService) {}
+
   async signup(dto: AuthDto) {
-    const hashedPassword = await argon2.hash(dto.password);
-
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hashedPassword,
-      },
-    });
-
-    delete user.hashedPassword;
-
-    return user;
+    try {
+      const hashedPassword = await argon2.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashedPassword,
+        },
+      });
+      delete user.hashedPassword;
+      return user;
+    } catch (err) {
+      if (err.code === 'P2002') {
+        throw new ForbiddenException('Email já se encontra em uso.');
+      }
+      throw err;
+    }
   }
   signin() {
     return 'Login';
