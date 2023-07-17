4afc4c6c823492869715ae6a63621c27429474cd
Francisco Costa <francisco100eg@gmail.com>
2023-07-17 18:42:38 -0300

    Fix: Config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { PrismaModule } from './prisma/prisma.module';
 import AuthModule from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule, RecipeModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    RecipeModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
