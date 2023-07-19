90c6f22f9fb55a12ee2d6f2ed33755862cafb779
Francisco Costa <francisco100eg@gmail.com>
2023-07-18 21:16:00 -0300

    Feat: Basic styles for navabar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
+    <main className='Home'>
       <h1>Home</h1>
     </main>
   )
--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import './Navbar.scss'
 
+import Image from 'next/image'
+import Link from 'next/link'
+
+const currentUser = {
+    firstName: 'John',
+    lastName: 'Doe',
+    image: ''
+}
 
 const Navbar = () => {
     return (
-        <nav>Navbar</nav>
+        <nav className='Navbar'>
+            <div className='Navbar__container'>
+                <div className='Navbar__left'>
+                    <div className='Navbar__logo'>
+                        <Image src='/assets/logo.png' alt='logo' fill className='logo__image' />
+                    </div>
+                    <div className='Navbar__title'>
+                        <h1><span>Master</span>Receita</h1>
+                    </div>
+                </div>
+                <div className='Navbar__right'>
+                    <div className='Navbar__cadastrar'>
+                        <Link href={"/"} className='Navbar__link'>Cadastrar</Link >
+                    </div>
+                    <div className='Navbar__user-name'>
+
+                        {currentUser.firstName && (
+                            <h1>
+                                {currentUser.firstName}
+                                {currentUser.lastName}
+                            </h1>
+                        )
+                        }
+                        <div className="Navabar__user-img">
+                            <Image src='/assets/profile.png' key={"user"} alt="user" fill className='user__image' />
+                        </div>
+                    </div>
+                    {
+                        (currentUser === undefined) ?
+                            (<button>
+                                Entrar
+                            </button>) :
+                            <button>
+                                Sair
+                            </button>
+                    }
+                </div>
+            </div>
+        </nav>
     )
 }
 
